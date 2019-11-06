5b78976bc971c177faa9e205e0694e1ba9e0181f
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-11-06 16:09:40 +0100

    use centralized android cache package

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/bitrise-io/go-android/cache"
+	"github.com/bitrise-io/go-android/gradle"
+	"github.com/bitrise-io/go-steputils/stepconf"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-io/go-utils/sliceutil"
-	"github.com/bitrise-steplib/bitrise-step-android-unit-test/cache"
-	"github.com/bitrise-tools/go-android/gradle"
-	"github.com/bitrise-tools/go-steputils/stepconf"
 	shellquote "github.com/kballard/go-shellquote"
 )
 
